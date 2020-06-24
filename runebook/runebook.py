920dfa442ac92e7835c1bb240b57d18e7c1446de
Sova <innominee@gmail.com>
2020-06-24 22:20:27 +0300

    Corrections, added finding runebook gump id

---
+++ runebook/runebook.py
@@ -0,0 +1,64 @@
+import re
+from py_stealth import *
+class Runebook:
+    ''' Runebook class, requires runebook serial to be provdided '''
+
+    def __init__(self, runebook_serial):
+        ''' Init method, checks if runebook exists '''
+        # Close all gumps ( including opened runebooks etc )
+        self._close_all_gumps()
+
+        if IsObjectExists(runebook_serial):
+            self.runebook_serial = runebook_serial
+        else:
+           raise ValueError("[Runebook] No runebook found!")
+
+        _runebook_gump_id = self._find_runebook_gump_id()
+        if _runebook_gump_id > 0:
+            self.runebook_gump_id = _runebook_gump_id
+        else:
+            raise RuntimeError("[Runebook] Failed to get runebook gump id")
+
+
+    def _find_runebook_gump_id(self) -> int:
+        ''' Find runebook gump index ( using Rename book anchor ) '''
+        self.open_runebook()
+        for _index in range(0, GetGumpsCount()):
+            for _text_line in GetGumpTextLines(_index):
+                if re.match(r"Rename\sbook", _text_line):
+                    print(f"[Runebook] Runebook gump id found! {hex(GetGumpID(_index))}")
+                    return GetGumpID(_index)           
+        return 0
+
+
+    def _close_all_gumps(self):
+        ''' Closes all gumps that can be closed by CloseSimpleGump '''
+        for _index in range(0, GetGumpsCount()):            
+            CloseSimpleGump(_index)
+            Wait(500)
+
+
+    def _press_gump_button(self, button=0):
+        ''' Press specified button by id in runebook '''
+        for _index in range(0, GetGumpsCount()):            
+            if GetGumpID(_index) == self.runebook_gump_id:
+                print("Found!")                
+                NumGumpButton(_index, button)                
+                Wait(500)
+
+
+    def open_runebook(self):
+        ''' Opens runebook '''
+        _current_gumps_count = GetGumpsCount()
+        _try = 0
+        while _current_gumps_count == GetGumpsCount():
+            _try += 1
+            if _try > 20:
+                raise RuntimeError("[Runebook] Failed to open runebook")
+            UseObject(self.runebook_serial)
+            Wait(1000)
+        
+
+    def recall(self, rune: int) -> bool:
+        ''' Recalls to selected rune '''
+        self._press_gump_button(1)
